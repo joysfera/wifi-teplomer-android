a30b86123c39aa267bd1407bc941407021329fb8
Petr Stehlík <petr@pstehlik.cz>
2018-08-05 08:51:52 +0200

    Do NOT turn Timer off on Screen Off event, just in case the broadcast receiver is killed and future Screen On events will not come. The Timer is put to sleep automatically by Android so no need to turn it off manually.

--- app/src/main/java/cz/pstehlik/wifiteplomer/MyBroadcastReceiver.java
+++ app/src/main/java/cz/pstehlik/wifiteplomer/MyBroadcastReceiver.java
@@ -14,17 +14,23 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     public static void registerScreenReceiver(Context context) {
         if (inst == null) {
             inst = new MyBroadcastReceiver();
+            Log.d("MyBroadcastReceiver", "creating new MyBroadcastReceiver");
+        } else {
+            // better unregister first so it handles repeated calls to register
+            unregisterScreenReceiver(context);
         }
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_USER_PRESENT);
         context.getApplicationContext().registerReceiver(inst, filter);
+        Log.d("MyBroadcastReceiver", "registerScreenReceiver");
     }
 
     public static void unregisterScreenReceiver(Context context) {
         if (inst != null) {
             context.getApplicationContext().unregisterReceiver(inst);
+            Log.d("MyBroadcastReceiver", "unregisterScreenReceiver");
         }
     }
 
@@ -33,7 +39,7 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         String action = (intent != null ? intent.getAction() : "");
         if (action.equals(Intent.ACTION_SCREEN_OFF)) {
             Log.d("MyBroadcastReceiver", "ACTION_SCREEN_OFF");
-            WidgetProvider.turnAlarmOnOff(context, false);
+            // WidgetProvider.turnAlarmOnOff(context, false);
         } else if (action.equals(Intent.ACTION_SCREEN_ON)) {
             Log.d("MyBroadcastReceiver", "ACTION_SCREEN_ON");
             if (!skipScreenOn)
